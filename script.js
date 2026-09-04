9ac5b0e7d08b3f73232d71dac5fe9c7547912313
Rajeev Kumar Kaushal <165012448+KaushalRaj2@users.noreply.github.com>
2026-09-05 04:49:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function firstWord(s) {
   // your code here
+	let st=s.trim();
+	let arr=st.split(" ");
+	let result="";
+	result+=arr[0];
+	return result;
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
